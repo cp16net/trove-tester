85075b46178a7a03d269bd26fe742df8b27a1fc1
Craig Vyvial <cp16net@gmail.com>
2015-12-03 19:20:22 -0600

    need to enable the trusty backports to install

--- prep.sh
+++ prep.sh
@@ -38,6 +38,7 @@ EOF
 }
 
 function install_dependencies() {
+    printf "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse\n" > /etc/apt/sources.list.d/backports.list
     apt-get update
     apt-get -y install git curl wget build-essential python-mysqldb \
         python-dev libssl-dev python-pip git-core libxml2-dev libxslt-dev \
